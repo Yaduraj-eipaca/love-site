11ae077e7f0e29e5f5302d9024a08a34ebfb970d
Yaduraj Sharma <yadurajsharma@MacBookPro.local>
2025-08-30 01:16:08 +0200

    Set Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/love-site/",   // 👈 important: matches your repo name
 })
